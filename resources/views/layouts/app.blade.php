428bd9ac83c070e4c42b72e6ac674e2322cd06e9
GiantCherry <lunev1981@mail.ru>
2019-08-16 21:41:27 +0200

    Модель (Model)

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,11 +1,20 @@
-<html>
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <head>
-    <title>Hexlet Blog - @yield('title')</title>
-    @include('shared.metatags')
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Laravel</title>
 </head>
 <body>
-<div class="container">
+<a href="/">Home</a>
+<a href="/about">About</a>
+<a href="/articles">Articles</a>
+
+<!-- BEGIN (write your solution here) -->
+<h1>@yield('header')</h1>
+<div>
     @yield('content')
 </div>
+<!-- END -->
 </body>
 </html>
