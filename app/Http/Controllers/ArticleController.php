734ab6d70d7e164f321660f0d5fc27d71eede4ad
GiantCherry <lunev1981@mail.ru>
2019-08-24 18:26:49 +0200

    Ресурсный роутинг

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -2,26 +2,29 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
 use App\Article;
+use Illuminate\Http\Request;
 
 class ArticleController extends Controller
 {
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
     public function index()
     {
         $articles = Article::paginate();
-
         // Статьи передаются в шаблон
         // compact('articles') => [ 'articles' => $articles ]
         return view('article.index', compact('articles'));
     }
 
-    public function show($id)
-    {
-        $article = Article::findOrFail($id);
-        return view('article.show', compact('article'));
-    }
-
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
     public function create()
     {
         // Передаем в шаблон вновь созданный объект. Он нужен для вывода формы через Form::model
@@ -29,7 +32,13 @@ class ArticleController extends Controller
         return view('article.create', compact('article'));
     }
 
-    // Здесь нам понадобится объект запроса, для извлечения данных
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
     public function store(Request $request)
     {
         // Проверка введенных данных
@@ -38,24 +47,47 @@ class ArticleController extends Controller
             'name' => 'required|unique:articles',
             'body' => 'required|min:1000',
         ]);
-
         $article = new \App\Article();
         // Заполнение статьи данными из формы
         $article->fill($request->all());
         // При ошибках сохранения возникнет исключение
         $article->save();
-
         // Редирект на указанный маршрут с добавлением флеш сообщения
         return redirect()
             ->route('articles.index');
-      }
+    }
 
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Article  $article
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $article = Article::findOrFail($id);
+        return view('article.show', compact('article'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Article  $article
+     * @return \Illuminate\Http\Response
+     */
     public function edit($id)
     {
         $article = Article::findOrFail($id);
         return view('article.edit', compact('article'));
     }
 
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Article  $article
+     * @return \Illuminate\Http\Response
+     */
     public function update(Request $request, $id)
     {
         $article = Article::findOrFail($id);
@@ -63,13 +95,18 @@ class ArticleController extends Controller
             'name' => 'required|unique:articles,' . $article->id,
             'body' => 'required|min:100',
         ]);
-
         $article->fill($request->all());
         $article->save();
         return redirect()
             ->route('articles.index');
     }
 
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Article  $article
+     * @return \Illuminate\Http\Response
+     */
     public function destroy($id)
     {
         $article = Article::findOrFail($id);
--- routes/web.php
+++ routes/web.php
@@ -15,23 +15,4 @@ Route::get('/', 'MainController@main');
 
 Route::get('/about', 'PageController@about');
 
-Route::get('/articles/create', 'ArticleController@create')
-    ->name('articles.create');
-
-Route::get('/articles', 'ArticleController@index')
-    ->name('articles.index');
-
-Route::post('/articles', 'ArticleController@store')
-    ->name('articles.store');
-
-Route::delete('/articles/{id}', 'ArticleController@destroy')
-    ->name('articles.destroy');
-
-Route::patch('/articles/{id}', 'ArticleController@update')
-    ->name('articles.update');
-
-Route::get('/articles/{id}', 'ArticleController@show')
-    ->name('articles.show');
-
-Route::get('/articles/{id}/edit', 'ArticleController@edit')
-    ->name('articles.edit');
+Route::resource('/articles', 'ArticleController');
